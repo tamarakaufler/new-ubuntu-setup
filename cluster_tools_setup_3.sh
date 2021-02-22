3ff1604ee8b14dc008f86944b9649bb4c4207485
Tamara Kaufler <tkaufler@gmail.com>
2021-02-22 18:49:42 +0000

    adds docker-compose install

--- cluster_tools_setup_3.sh
+++ cluster_tools_setup_3.sh
@@ -14,7 +14,7 @@ sudo add-apt-repository \
    $(lsb_release -cs) \
    stable"
 sudo apt-get update
-sudo apt-get install docker-ce docker-ce-cli containerd.io
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose
 
 sudo usermod -aG docker $USER
 su -s $USER
