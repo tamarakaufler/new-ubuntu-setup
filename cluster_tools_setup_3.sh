360e0a6fc6aacfb0a87936c2f255594633c78e8e
Tamara Kaufler <tkaufler@gmail.com>
2021-10-18 18:53:59 +0100

    replaces docker-compose with version 2 as a docker plugin

--- cluster_tools_setup_3.sh
+++ cluster_tools_setup_3.sh
@@ -12,13 +12,19 @@ sudo add-apt-repository \
    $(lsb_release -cs) \
    stable"
 sudo apt-get update
-sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io
 
 sudo usermod -aG docker $USER
 su -s $USER
 if [ $? != 0 ]; then
     echo "Root password is not set. Docker access will be granted on next your next login"
 fi
+
+# docker compose v2
+# usage: docker compose version (instead of docker-compose)
+mkdir -p ~/.docker/cli-plugins/
+curl -SL https://github.com/docker/compose/releases/download/v2.0.1/docker-compose-linux-x86_64 -o ~/.docker/cli-plugins/docker-compose
+chmod +x ~/.docker/cli-plugins/docker-compose
 
 # KUBECTL + HELM
 # --------------
