54ca8da613fefe683018a2ce4936b78d99fe02ac
Tamara Kaufler <tkaufler@gmail.com>
2022-08-04 13:27:36 +0100

    adds installation of a tool for AWS rotation

---
+++ cluster_tools._AWS_3c.sh
@@ -0,0 +1,5 @@
+# tool for rotation of AWS IAM keys
+# 	https://github.com/rhyeal/aws-rotate-iam-keys
+
+brew tap rhyeal/aws-rotate-iam-keys https://github.com/rhyeal/aws-rotate-iam-keys
+brew install aws-rotate-iam-keys
