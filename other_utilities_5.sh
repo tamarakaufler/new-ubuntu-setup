f3d406061ebd1e8f4db6bfc7510fb0ed77282dc5
Tamara Kaufler <tkaufler@gmail.com>
2023-01-17 11:50:48 +0000

    adds ngrok installation

--- other_utilities_5.sh
+++ other_utilities_5.sh
@@ -2,6 +2,13 @@
 cd ~ && wget -O - "https://www.dropbox.com/download?plat=lnx.x86_64" | tar xzf -
 # ~/.dropbox-dist/dropboxd
 
+# ngrok
+curl -s https://ngrok-agent.s3.amazonaws.com/ngrok.asc | \
+  sudo tee /etc/apt/trusted.gpg.d/ngrok.asc >/dev/null && \
+  echo "deb https://ngrok-agent.s3.amazonaws.com buster main" | \
+  sudo tee /etc/apt/sources.list.d/ngrok.list && \
+  sudo apt update && sudo apt install ngrok
+
 # notebook
 sudo snap install cherrytree
 
